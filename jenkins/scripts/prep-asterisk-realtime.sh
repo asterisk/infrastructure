051149ace0ff6a58ca492f754a9f2cb020bcdcfc
Joshua Colp <jcolp@digium.com>
2016-02-01 11:01:35 -0400

    realtime: Add a script to Alembic upgrade realtime database for testing.
    This change adds a script which drops all existing tables for a realtime
    database and recreates it up to head using alembic. This is used by the
    realtime job to ensure the database is as expected.
    
    Change-Id: Ic4989e90d9ed2492cc89c98cb95468b5dd2c8e6d

---
+++ jenkins/scripts/prep-asterisk-realtime.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+#
+# Create an Alembic configuration file and upgrade a Postgresql database to HEAD
+#
+# Copyright 2016 (C), Digium, Inc.
+# Joshua Colp <jcolp@digium.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#	http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+DB_USER=$1
+DB_HOST=$2
+DB_NAME=$3
+
+if [ -z "$DB_USER" ]; then
+    echo "The database username must be the first argument."
+    exit 1
+fi
+
+if [ -z "$DB_HOST" ]; then
+    echo "The database host must be the second argument."
+    exit 1
+fi
+
+if [ -z "$DB_NAME" ]; then
+    echo "The database name must be the third argument."
+    exit 1
+fi
+
+# Drop any existing tables we have created from a previous run to ensure the alembic has to run all revisions
+psql --username=${DB_USER} --host=${DB_HOST} --db=${DB_NAME} --command="DROP OWNED BY ${DB_USER}"
+
+cat > config.ini << EOF
+[alembic]
+script_location = asterisk/ast-db-manage/config
+sqlalchemy.url = postgresql://${DB_USER}@${DB_HOST}/${DB_NAME}
+
+[loggers]
+keys = root,sqlalchemy,alembic
+
+[handlers]
+keys = console
+
+[formatters]
+keys = generic
+
+[logger_root]
+level = WARN
+handlers = console
+qualname =
+
+[logger_sqlalchemy]
+level = WARN
+handlers =
+qualname = sqlalchemy.engine
+
+[logger_alembic]
+level = INFO
+handlers =
+qualname = alembic
+
+[handler_console]
+class = StreamHandler
+args = (sys.stderr,)
+level = NOTSET
+formatter = generic
+
+[formatter_generic]
+format = %(levelname)-5.5s [%(name)s] %(message)s
+datefmt = %H:%M:%S
+
+EOF
+
+echo "Creating database tables"
+alembic -c config.ini upgrade HEAD
+
+rm -rf config.ini
+
+echo "*** Asterisk Realtime Database Ready ***"
+
