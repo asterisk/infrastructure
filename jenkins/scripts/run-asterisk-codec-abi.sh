fc52deb5667b3860681fb67b2d6c0d21220e220e
Sean Bright <sean.bright@gmail.com>
2017-03-23 11:22:35 -0400

    REALLY correct the URL to the codec_silk download
    Forgot to append the .0 to the branch number
    
    Change-Id: If26952dc2a9e756351d01470790761c42bf3cbe5

--- jenkins/scripts/run-asterisk-codec-abi.sh
+++ jenkins/scripts/run-asterisk-codec-abi.sh
@@ -94,7 +94,7 @@ install_codecs() {
 	# Lowercase the 'X'
 	ARCH=${ARCH,X}
 
-	TARGET_DIR=${CODEC}-${BRANCH}_current-${ARCH}_${BITS}
+	TARGET_DIR=${CODEC}-${BRANCH}.0_current-${ARCH}_${BITS}
 
 	# Any extra flags needed for wget?
 	wget http://downloads.digium.com/pub/telephony/${CODEC}/asterisk-${BRANCH}.0/${ARCH}-${BITS}/${TARGET_DIR}.tar.gz
