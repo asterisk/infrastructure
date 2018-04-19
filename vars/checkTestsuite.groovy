b4bf134c2814445b683851e2d9cf0937e6955680
Corey Farrell <git@cfware.com>
2018-04-19 09:46:49 -0400

    checkTestsuite: Use self_test from testsuite.
    This reduces interdependencies between the jenkins job and the testsuite
    structure.  This makes it easier to change the structure of the
    testsuite unit tests (required for python3 compatibility).
    
    Change-Id: I611b4baea7c4907d01e0932873b049e697ca5195

--- vars/checkTestsuite.groovy
+++ vars/checkTestsuite.groovy
@@ -13,15 +13,7 @@ def call() {
 		checkoutGerritChange("testsuite")
 		stage("run-testsuite-unitests") {
 			dir("testsuite") {
-				def test_names = [
-					"buildoptions",
-					"channel_test_condition",
-					"sippversion"
-				]
-				for (test_name in test_names) {
-					echo " ==> Executing " + test_name
-					sh "python ./lib/python/asterisk/" + test_name + ".py"
-				}
+				sh "./self_test"
 				gerritverificationpublisher verifyStatusValue: 1, verifyStatusCategory: 'Passed',
 					verifyStatusComment: '${env.BUILD_TAG}', verifyStatusName: "${env.JOB_NAME}",
 					verifyStatusReporter: 'Jenkins2', verifyStatusRerun: 'recheck'
