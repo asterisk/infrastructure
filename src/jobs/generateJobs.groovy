f50935ebb3e82704b235c6296ce078014113a52c
George Joseph <gjoseph@digium.com>
2018-07-11 13:05:02 -0600

    Remove check and gate jobs
    Change-Id: I03faaae3b957ae99725aa926de449c948fae2546

--- src/jobs/generateJobs.groovy
+++ src/jobs/generateJobs.groovy
@@ -147,63 +147,6 @@ dashboardView('Periodics') {
 	}
 }
 
-println "Creating asterisk check jobs"
-for (br in globals.ast_branches) {
-	for (arch in br.value.arches) {
-		pipelineJob("check-ast-${br.key}-${arch}") {
-			definition {
-				cps {
-					script("""\
-						manager.build.displayName = "\${env.GERRIT_CHANGE_NUMBER}"
-						timestamps() {
-							node ('job:check && bits:${arch}') {
-								checkAsterisk('${br.key}', '${arch}')
-							}
-						}""")
-					sandbox(false)
-				}
-			}
-			logRotator {
-				daysToKeep(20)
-				artifactDaysToKeep(10)
-			}
-			triggers {
-				gerritTrigger {
-					serverName(br.value.gerrit_trigger)
-					silentMode(false)
-					silentStartMode(true)
-					gerritBuildFailedVerifiedValue(-1)
-					gerritBuildSuccessfulVerifiedValue(1)
-					gerritBuildUnstableVerifiedValue(-1)
-					notificationLevel("OWNER_REVIEWERS")
-					triggerOnEvents {
-						commentAddedContains { commentAddedCommentContains('^Patch Set [0-9]+:..recheck$') }
-						changeRestored()
-						draftPublished()
-						patchsetCreated {
-							excludeDrafts(false)
-							excludeTrivialRebase(false)
-							excludeNoCodeChange(false)
-						}
-					}
-					gerritProjects {
-						gerritProject {
-							compareType("REG_EXP")
-							pattern('^asterisk$')
-							branches {
-								branch {
-									compareType("REG_EXP")
-									pattern("(certified/)?${br.key}(.[0-9]+)?")
-								}
-							}
-							disableStrictForbiddenFileVerification(false)
-						}
-					}
-				}
-			}
-		}
-	}
-}
 
 println "Creating testsuite check jobs"
 for (br in globals.testsuite_branches) {
@@ -278,66 +221,6 @@ pipelineJob("check-testsuite-pep8") {
 	}
 }
 
-println "Creating asterisk gate jobs"
-for (br in globals.ast_branches) {
-	for (gt in br.value.gate_types) {
-		pipelineJob("gate-ast-${br.key}-${gt}") {
-			definition {
-				cps {
-					script("""\
-						manager.build.displayName = "\${env.GERRIT_CHANGE_NUMBER}"
-						timestamps() {
-							node ('job:gate') {
-								gateAsterisk('${br.key}', '${gt}')
-							}
-						}""")
-					sandbox(false)
-				}
-			}
-			logRotator {
-				daysToKeep(20)
-				artifactDaysToKeep(10)
-			}
-			triggers {
-				gerritTrigger {
-					serverName(br.value.gerrit_trigger)
-					silentMode(false)
-					silentStartMode(true)
-					gerritBuildFailedVerifiedValue(-1)
-					gerritBuildSuccessfulVerifiedValue(2)
-					gerritBuildUnstableVerifiedValue(-1)
-					notificationLevel("OWNER_REVIEWERS")
-					triggerOnEvents {
-						commentAdded {
-							verdictCategory("CodeReview")
-							commentAddedTriggerApprovalValue("+2")
-						}
-						commentAddedContains { commentAddedCommentContains('^Patch Set [0-9]+: Code-Review[+]2$') }
-						commentAddedContains { commentAddedCommentContains('^Patch Set [0-9]+:..regate$') }
-					}
-					gerritProjects {
-						gerritProject {
-							compareType("REG_EXP")
-							pattern('^asterisk$')
-							branches {
-								branch {
-									compareType("PLAIN")
-									pattern(br.key)
-								}
-								branch {
-									compareType("REG_EXP")
-									pattern("(certified/)?${br.key}(.[0-9]+)?")
-								}
-							}
-							disableStrictForbiddenFileVerification(false)
-						}
-					}
-				}
-			}
-
-		}
-	}
-}
 
 println "Creating testsuite gate job"
 pipelineJob("gate-testsuite") {
